94839ea0767bae4d229fc761f4fc1f24e15a9004
blue <g1157143860@gmail.com>
2025-10-28 20:14:17 +0800

    hk6-cargo_clean

--- homework/hk6/target/debug/deps/hk6-637c8723f0121e71.d
+++
@@ -1,5 +0,0 @@
-/home/blue/Rust_learning/homework/hk6/target/debug/deps/hk6-637c8723f0121e71.d: src/main.rs
-
-/home/blue/Rust_learning/homework/hk6/target/debug/deps/hk6-637c8723f0121e71: src/main.rs
-
-src/main.rs:
--- homework/hk6/target/debug/deps/hk6-66d154e098b180d1.d
+++
@@ -1,5 +0,0 @@
-/home/blue/Rust_learning/homework/hk6/target/debug/deps/hk6-66d154e098b180d1.d: src/main.rs
-
-/home/blue/Rust_learning/homework/hk6/target/debug/deps/hk6-66d154e098b180d1: src/main.rs
-
-src/main.rs:
